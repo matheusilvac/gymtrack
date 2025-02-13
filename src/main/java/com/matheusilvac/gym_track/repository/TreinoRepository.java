8f41aca8bba3489e583dc11dfe76cb45c47b6d30
matheuscs0 <dev.matheuscs@gmail.com>
2025-02-13 19:10:35 -0300

    fix: add exercicio

--- src/main/java/com/matheusilvac/gym_track/repository/TreinoRepository.java
+++ src/main/java/com/matheusilvac/gym_track/repository/TreinoRepository.java
@@ -1,10 +1,12 @@
 package com.matheusilvac.gym_track.repository;
 
 import com.matheusilvac.gym_track.domain.entity.treino.Treino;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface TreinoRepository extends JpaRepository<Treino, Long> {
-    Optional<Treino> findAllByOrderByDataAsc();
+    Page<Treino> findAllByOrderByDataAsc(Pageable paginacao);
 }
--- src/main/java/com/matheusilvac/gym_track/service/TreinoService.java
+++ src/main/java/com/matheusilvac/gym_track/service/TreinoService.java
@@ -1,21 +1,27 @@
 package com.matheusilvac.gym_track.service;
 
+import com.matheusilvac.gym_track.domain.dtos.CardioDTO;
 import com.matheusilvac.gym_track.domain.dtos.ExercicioDTO;
 import com.matheusilvac.gym_track.domain.dtos.SeriesDTO;
-import com.matheusilvac.gym_track.domain.dtos.TreinoDTO;
+import com.matheusilvac.gym_track.domain.entity.cardio.Cardio;
 import com.matheusilvac.gym_track.domain.entity.exercicio.Exercicio;
 import com.matheusilvac.gym_track.domain.entity.series.Series;
 import com.matheusilvac.gym_track.domain.entity.treino.Treino;
+import com.matheusilvac.gym_track.domain.enums.modalidadeCardio.ModalidadeCardio;
 import com.matheusilvac.gym_track.domain.enums.tipoSerieEnum.TipoSerieEnum;
+import com.matheusilvac.gym_track.repository.CardioRepository;
 import com.matheusilvac.gym_track.repository.ExercicioRepository;
 import com.matheusilvac.gym_track.repository.SeriesRepository;
 import com.matheusilvac.gym_track.repository.TreinoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
+@Service
 public class TreinoService {
     @Autowired
     private TreinoRepository treinoRepository;
@@ -26,23 +32,25 @@ public class TreinoService {
     @Autowired
     private SeriesRepository seriesRepository;
 
+    @Autowired
+    private CardioRepository cardioRepository;
+
     // Criar treino temporário
     public Treino criarTreinoTemporario(String nome) {
         // Criar um treino temporário (não finalizado)
         Treino treino = new Treino();
+        treino.setNome(nome);
         treino.setData(LocalDateTime.now());  // Data de criação do treino
         treino.setFinalizado(false);  // O treino ainda está em progresso
         treinoRepository.save(treino);
         return treino;
     }
 
-    public List<Treino> obterTodos(){
-        return treinoRepository.findAllByOrderByDataAsc()
-                .stream()
-                .map(treino -> new Treino(treino.getId(), treino.getData(), treino.getExercicios(), treino.getCardio()))
-                .collect(Collectors.toList());
+    public Page<Treino> obterTodos(Pageable paginacao){
+        return treinoRepository.findAllByOrderByDataAsc(paginacao);
     }
 
+    @Transactional
     // Adicionar exercício ao treino
     public Exercicio adicionarExercicio(Long treinoId, ExercicioDTO exercicioDTO) {
         Treino treino = treinoRepository.findById(treinoId).orElseThrow(() -> new RuntimeException("Treino não encontrado"));
@@ -52,22 +60,49 @@ public class TreinoService {
         }
 
         Exercicio exercicio = new Exercicio();
-        exercicio.setNome(exercicioDTO.nome());
+        exercicio.setNomeExercicio(exercicioDTO.getNomeExercicio());
         exercicio.setTreino(treino);
         exercicioRepository.save(exercicio);
 
         // Adicionando as séries
-        for (SeriesDTO seriesDTO : exercicioDTO.series()) {
+        for (SeriesDTO seriesDTO : exercicioDTO.getSeries()) {
             Series series = new Series();
-            series.setTipoSerieEnum(TipoSerieEnum.valueOf(seriesDTO.tipoSerieEnum()));
-            series.setRepeticao(seriesDTO.repeticao());
-            series.setCarga(seriesDTO.carga());
+            series.setTipoSerieEnum(TipoSerieEnum.valueOf(seriesDTO.getTipoSerieEnum()));
+            series.setRepeticao(seriesDTO.getRepeticao());
+            series.setCarga(seriesDTO.getCarga());
             series.setExercicio(exercicio);
+            System.out.println("Salvando série: " + series);
             seriesRepository.save(series);
         }
 
         return exercicio;
     }
+
+    //adicionar cardio
+    @Transactional
+    public Cardio adicionarCardio(Long treinoId, CardioDTO cardioDTO){
+        Treino treino = treinoRepository.findById(treinoId).orElseThrow(() -> new RuntimeException("Treino não encontrado"));
+        if (treino.isFinalizado()) {
+            throw new RuntimeException("Não é possível adicionar exercício a um treino finalizado.");
+        }
+        Cardio cardio = new Cardio();
+        cardio.setModalidadeCardio(ModalidadeCardio.valueOf(cardioDTO.getModalidadeCardio()));
+        cardio.setKm(cardioDTO.getKm());
+        cardio.setTempo(cardioDTO.getTempo());
+        if (cardioDTO.getKm() != null && cardioDTO.getKm() > 0) {
+            cardio.setPace((double) cardioDTO.getTempo() / cardioDTO.getKm());
+        } else {
+            cardio.setPace(null);
+        }
+
+        treino.setCardio(cardio);
+
+        cardioRepository.save(cardio);
+        treinoRepository.save(treino);
+
+        return cardio;
+    }
+
 
     // Finalizar treino
     public Treino finalizarTreino(Long treinoId) {
@@ -80,5 +115,9 @@ public class TreinoService {
         treino.setFinalizado(true);
         treinoRepository.save(treino);
         return treino;
+    }
+
+    public void excluirTreino(Long id) {
+        treinoRepository.deleteById(id);
     }
 }
